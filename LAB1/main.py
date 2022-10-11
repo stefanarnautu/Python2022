78bd9e66a34957965be3cca8f2d21e4c0098cd24
stefanarnautu <stefanarnautu01@gmail.com>
2022-10-11 09:54:40 +0300

    Am adaugat 5 comentat

--- LAB1/main.py
+++ LAB1/main.py
@@ -46,15 +46,41 @@ for i in test_str:
 print("The uppercase characters in string are : " + result)
 print("\n5-------------");
 #5
-def spiral (M):
-  print(M);
-
-MAT = [['f','i','r','s'],
-       ['n','_','l','t'],
-       ['o','b','a','_'],
-       ['h','t','y','p']]
-
-
+# def spiralOrder(matrix):
+#     spiralWord = []
+#
+#     if (len(matrix) == 0):
+#         return spiralWord
+#
+#     m = len(matrix)
+#     n = len(matrix[0])
+#     seen = [[0 for i in range(n)] for j in range(m)]
+#     dr = [0, 1, 0, -1]
+#     dc = [1, 0, -1, 0]
+#     x = 0
+#     y = 0
+#     di = 0
+#
+#     for i in range(m * n):
+#         spiralWord.append(matrix[x][y])
+#         seen[x][y] = True
+#         cr = x + dr[di]
+#         cc = y + dc[di]
+#
+#         if (0 <= cr and cr < m and 0 <= cc and cc < n and not (seen[cr][cc])):
+#             x = cr
+#             y = cc
+#         else:
+#             di = (di + 1) % 4
+#             x += dr[di]
+#             y += dc[di]
+#     return spiralWord
+#
+# a = [["f","i","r","s"],["n","_","l","t"],["o","b","a","_"],["h","t","y","p"]]
+#
+# for x in spiralOrder(a):
+#     print(x, end="")
+# print()
 
 
 print("\n6-------------");
